8c0fbc36f27a6f5821c9d1572a361ae99d977f14
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2016-07-06 17:40:03 +0200

    Refactor to avoid long lines

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,9 @@ import qualified Data.List as L
 import qualified Data.Text as T
 
 main :: IO ()
-main = getContents >>= sentiment' . lines >>= printSentiments
+main = getContents
+    >>= sentiment' . lines
+    >>= either print (`forM_` printSentiment)
 
 sentiment' :: [String] -> IO (Either SentimentException [Sentiment])
 sentiment' = sentiment endpoint apiKey . map C.pack
@@ -22,15 +24,13 @@ apiKey :: String
 apiKey = error "Enter credentials"
 
 endpoint :: String
---endpoint = "http://localhost:1337"
 endpoint = "https://ussouthcentral.services.azureml.net/workspaces/c788c20533a549f78f520f17b4b17f39/services/fe5d1e3a57a04236892b69f36a0de3ef/execute?api-version=2.0&details=true"
 
-printSentiments :: Either SentimentException [Sentiment] -> IO ()
-printSentiments (Left err) = print err
-printSentiments (Right xs) = xs `forM_` printS
-
-printS :: Sentiment -> IO ()
-printS s = printf "[%s (%.2f%%)] %s\n" (label s) (conf s * 100) (ellipsis . map spaces . T.unpack . query $ s)
+printSentiment :: Sentiment -> IO ()
+printSentiment s = printf "[%s (%.2f%%)] %s\n"
+    (label s)
+    (conf s * 100)
+    (ellipsis . map spaces . T.unpack . query $ s)
 
 spaces :: Char -> Char
 spaces c
